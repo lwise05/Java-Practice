62b354896a916cf41a9b84cf0e16629a59367d61
Leslie <leslie_aldaz@hotmail.com>
2025-06-05 15:48:30 -0600

    Caesar cipher is able to shift positive or negative

--- CipherApp/src/CaesarCipher/Cipher.java
+++ CipherApp/src/CaesarCipher/Cipher.java
@@ -3,56 +3,48 @@ import java.util.Scanner;
 
 public class Cipher {
 
-    public static void main(String[] args) {
-        System.out.println("Enter text to encrypt: ");
-        Scanner sc = new Scanner(System.in);
-        String textInput = sc.nextLine();
-        System.out.println("Enter # of shifts (0-25): ");
-        int shiftInput = sc.nextInt();
-        sc.nextLine();
-        
-        System.out.println(encrypt(textInput, shiftInput));
-//        System.out.println(encrypt("Abc 10 LkxZe ne!a", 10));
-    }
-    
-    public static String encrypt (String text, int shift) {
-        StringBuilder result = new StringBuilder();
-        
-        for (char character : text.toCharArray()) {
-            if (Character.isLetter(character)) {
-                int charNum = (int)character;
-                int newNum = charNum + shift;
-                    
-                if (Character.isLowerCase(character)) {
-                    if (newNum > 'z') {
-                        int remainder = newNum - 'z';
-                        character = (char)('`' + remainder);
-                    }
-                    else {
-                        character = (char)newNum;
-                    }
-                    
-                }
-                
-                else if (Character.isUpperCase(character) ) {
-                    if (newNum - 'Z' >= 0) {
-                        int remainder = newNum - 'Z';
-                        character = (char) ('@' + remainder);
-                    }
-                    else {
-                        character = (char)newNum;
-                    }
-                }
+	 private static String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+	    private static String upperCase;
 
-                result.append(character);
-                
-            }
-            else {
-                result.append(character);
-            }
-        }
-        
-        return result.toString();
-    }
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        upperCase = lowerCase.toUpperCase();
+
+	        System.out.println("Enter text to encrypt: ");
+	        String textInput = sc.nextLine();
+
+	        System.out.println("Enter # of shifts (0-25): ");
+	        int shiftInput = sc.nextInt();
+	        sc.nextLine();
+
+	        System.out.println(encrypt(textInput, shiftInput));
+	        sc.close();
+	    }
+
+	    public static String encrypt(String text, int shift) {
+	        StringBuilder result = new StringBuilder();
+
+	        for (char character : text.toCharArray()) {
+	            String charSet = Character.isLowerCase(character) ? lowerCase : upperCase;
+	            if (Character.isLetter(character)) {
+	                int shiftedIndex = (charSet.indexOf(character) + shift) % 26;
+
+	                if (shift < 0) {
+	    
+	                    if (shift + charSet.indexOf(character) < 0) {
+	                        shiftedIndex = 26 + shiftedIndex;
+	                    }
+
+	                }
+	                result.append(charSet.charAt(shiftedIndex));
+
+	            } else {
+	                result.append(character);
+
+	            }
+	        }
+
+	        return result.toString();
+	    }
 
 }
